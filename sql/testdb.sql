aa2906b5abad6b798ec44ec23cc3293dd5b2e4ca
ibukiyabu <ibuki.y2115@gmail.com>
2024-02-15 15:03:02 +0900

    一部修正

--- sql/testdb.sql
+++ sql/testdb.sql
@@ -1,25 +1,25 @@
-drop database if exist testdb;
+drop database if exists testdb;
 create database testdb;
 use testdb;
 create table users
 (
-   user id int primary key auto_increment,
+   user_id int primary key auto_increment,
    user_name varchar (255),
-   password varchar (255),
+   password varchar (255)
 );
-insert into user values
+insert into users values
 (
    1,
    "taro",
    "123"
 );
-insert into user values
+insert into users values
 (
    2,
    "jiro",
    "456"
 );
-insert into user values
+insert into users values
 (
    3,
    "hanako",
